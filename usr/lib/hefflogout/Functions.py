46a640fc7afed81291a1de465467cd14ac01c736
Brad Heffernan <ph3onix83@gmail.com>
2020-04-02 15:00:02 +1030

    updated

--- usr/lib/hefflogout/Functions.py
+++ usr/lib/hefflogout/Functions.py
@@ -16,25 +16,36 @@ config = "/etc/hefflogout.conf"
 # config = ''.join([str(Path(__file__).parents[3]), "/etc/hefflogout.conf"])
 
 
+def cache_bl(self, GLib, Gtk):
+    if os.path.isfile("/usr/bin/betterlockscreen"):
+        subprocess.run(["betterlockscreen", "-u",
+                        self.wallpaper],
+                       shell=False)
+        GLib.idle_add(self.lbl_stat.set_text, "")
+        os.unlink("/tmp/hefflogout.lock")
+        os.system(self.cmd_lock)
+        Gtk.main_quit()
+    else:
+        print("not installed betterlockscreen.")
+
+
 def get_config(self, Gdk, config):
     self.parser = configparser.SafeConfigParser()
     self.parser.read(config)
 
     # Set some safe defaults
     self.opacity = 0.6
-    self.bgcolor = "#000000"
-    self.r = 0
-    self.g = 0
-    self.b = 0
 
     # Check if we're using HAL, and init it as required.
     if self.parser.has_section("settings"):
-        # if self.parser.has_option("settings", "backend"):
-        #     self.bgcolor = self.parser.get("settings", "backend")
-        #     hex2rgb(self, self.bgcolor.replace('#', ''))
         if self.parser.has_option("settings", "opacity"):
             self.opacity = int(self.parser.get("settings", "opacity"))/100
+        if self.parser.has_option("settings", "lock_wallpaper"):
+            self.wallpaper = self.parser.get("settings", "lock_wallpaper")
 
+    if self.parser.has_section("commands"):
+        if self.parser.has_option("commands", "lock"):
+            self.cmd_lock = self.parser.get("commands", "lock")
 
 def _get_logout():
     out = subprocess.run(["sh", "-c", "env | grep DESKTOP_SESSION"],
@@ -59,7 +70,14 @@ def _get_logout():
         return "pkill dwm"
     elif desktop in ("i3", "/usr/share/xsessions/i3"):
         return "pkill i3"
+    elif desktop in ("spectrwm", "/usr/share/xsessions/spectrwm"):
+        return "pkill spectrwm"
     # elif desktop in ("xfce", "/usr/share/xsessions/xfce"):
     #     return "xfce4-session-logout --logout"
 
     return None
+
+
+def file_check(file):
+    if os.path.isfile(file):
+        return True
--- usr/lib/hefflogout/hefflogout.py
+++ usr/lib/hefflogout/hefflogout.py
@@ -6,26 +6,28 @@ import cairo
 import gi
 import GUI
 import Functions as fn
+import threading
 
 gi.require_version('Gtk', '3.0')
 gi.require_version('Gdk', '3.0')
+gi.require_version('Wnck', '3.0')
 
-from gi.repository import Gtk, GdkPixbuf
-from gi.repository import Gdk, Gdk
+from gi.repository import Gtk, GdkPixbuf, Gdk, Wnck, GLib, GdkX11  # noqa
 
 
 class TransparentWindow(Gtk.Window):
     cmd_shutdown = "systemctl poweroff"
     cmd_restart = "systemctl reboot"
     cmd_suspend = "systemctl suspend"
+    cmd_hibernate = "systemctl hibernate"
     cmd_lock = "betterlockscreen -l dimblur"
+    wallpaper = ""
 
     def __init__(self):
-        Gtk.Window.__init__(self)
-
+        super(TransparentWindow, self).__init__(title="Arcolinux Logout")
         self.set_size_request(300, 220)
-
-        self.connect('destroy', Gtk.main_quit)
+        self.monitor = 0
+        self.connect('delete-event', self.on_close)
         self.connect('draw', self.draw)
         self.connect("key-press-event", self.on_keypress)
         self.connect("window-state-event", self.on_window_state_event)
@@ -33,6 +35,16 @@ class TransparentWindow(Gtk.Window):
         self.set_position(Gtk.WindowPosition.CENTER)
 
         screen = self.get_screen()
+
+        screens = Gdk.Display.get_default()
+        monitor = screens.get_monitor(0)
+        rect = monitor.get_geometry()
+
+        width = rect.width
+        height = rect.height
+
+        self.resize(width, height)
+
         visual = screen.get_rgba_visual()
         if visual and screen.is_composited():
             self.set_visual(visual)
@@ -42,7 +54,10 @@ class TransparentWindow(Gtk.Window):
         self.fullscreen()
         self.set_app_paintable(True)
         GUI.GUI(self, Gtk, GdkPixbuf, fn.working_dir, fn.os, Gdk)
-        self.show_all()
+
+        if not fn.os.path.isfile("/tmp/hefflogout.lock"):
+            with open("/tmp/hefflogout.lock", "w") as f:
+                f.write("")
 
     def on_mouse_in(self, widget, event, data):
         if data == "S":
@@ -65,6 +80,15 @@ class TransparentWindow(Gtk.Window):
             plo = GdkPixbuf.Pixbuf().new_from_file_at_size(
                 fn.os.path.join(fn.working_dir, 'logout_blur.svg'), 64, 64)
             self.imagelo.set_from_pixbuf(plo)
+        elif data == "Escape":
+            plo = GdkPixbuf.Pixbuf().new_from_file_at_size(
+                fn.os.path.join(fn.working_dir, 'cancel_blur.svg'), 64, 64)
+            self.imagec.set_from_pixbuf(plo)
+        elif data == "H":
+            plo = GdkPixbuf.Pixbuf().new_from_file_at_size(
+                fn.os.path.join(fn.working_dir, 'hibernate_blur.svg'), 64, 64)
+            self.imageh.set_from_pixbuf(plo)
+        event.window.set_cursor(Gdk.Cursor(Gdk.CursorType.HAND2))
 
     def on_mouse_out(self, widget, event, data):
         if data == "S":
@@ -87,12 +111,20 @@ class TransparentWindow(Gtk.Window):
             plo = GdkPixbuf.Pixbuf().new_from_file_at_size(
                 fn.os.path.join(fn.working_dir, 'logout.svg'), 64, 64)
             self.imagelo.set_from_pixbuf(plo)
+        elif data == "Escape":
+            plo = GdkPixbuf.Pixbuf().new_from_file_at_size(
+                fn.os.path.join(fn.working_dir, 'cancel.svg'), 64, 64)
+            self.imagec.set_from_pixbuf(plo)
+        elif data == "H":
+            plo = GdkPixbuf.Pixbuf().new_from_file_at_size(
+                fn.os.path.join(fn.working_dir, 'hibernate.svg'), 64, 64)
+            self.imageh.set_from_pixbuf(plo)
 
     def on_click(self, widget, event, data):
         self.click_button(widget, data)
 
     def on_window_state_event(self, widget, ev):
-        self.__is_fullscreen = bool(ev.new_window_state & Gdk.WindowState.FULLSCREEN)
+        self.__is_fullscreen = bool(ev.new_window_state & Gdk.WindowState.FULLSCREEN)  # noqa
 
     def draw(self, widget, context):
         context.set_source_rgba(0, 0, 0, self.opacity)
@@ -110,28 +142,60 @@ class TransparentWindow(Gtk.Window):
     def click_button(self, widget, data=None):
         if (data == 'L'):
             command = fn._get_logout()
-            self.__exec_cmd(command)            
+            fn.os.unlink("/tmp/hefflogout.lock")
+            self.__exec_cmd(command)
+            Gtk.main_quit()
 
         elif (data == 'R'):
+            fn.os.unlink("/tmp/hefflogout.lock")
             self.__exec_cmd(self.cmd_restart)
+            Gtk.main_quit()
 
         elif (data == 'S'):
+            fn.os.unlink("/tmp/hefflogout.lock")
             self.__exec_cmd(self.cmd_shutdown)
+            Gtk.main_quit()
 
         elif (data == 'U'):
+            fn.os.unlink("/tmp/hefflogout.lock")
             self.__exec_cmd(self.cmd_suspend)
+            Gtk.main_quit()
 
         elif (data == 'H'):
+            fn.os.unlink("/tmp/hefflogout.lock")
             self.__exec_cmd(self.cmd_hibernate)
+            Gtk.main_quit()
 
         elif (data == 'K'):
-            self.__exec_cmd(self.cmd_lock)
-
-        Gtk.main_quit()
+            if not fn.os.path.isdir(fn.home + "/.cache/i3lock"):
+                self.lbl_stat.set_markup("<span size=\"x-large\"><b>Caching lockscreen images for a faster locking next time</b></span>")  # noqa
+                t = threading.Thread(target=fn.cache_bl,
+                                     args=(self, GLib, Gtk,))
+                t.daemon = True
+                t.start()
+            else:
+                fn.os.unlink("/tmp/hefflogout.lock")
+                self.__exec_cmd(self.cmd_lock)
+                Gtk.main_quit()
+        else:
+            fn.os.unlink("/tmp/hefflogout.lock")
+            Gtk.main_quit()
 
     def __exec_cmd(self, cmdline):
         fn.os.system(cmdline)
 
+    def on_close(self, widget, data):
+        fn.os.unlink("/tmp/hefflogout.lock")
+        Gtk.main_quit()
 
-TransparentWindow()
-Gtk.main()
+
+if __name__ == "__main__":
+    if not fn.os.path.isfile("/tmp/hefflogout.lock"):
+        with open("/tmp/hefflogout.pid", "w") as f:
+            f.write(str(fn.os.getpid()))
+            f.close()
+        w = TransparentWindow()
+        w.show_all()
+        Gtk.main()
+    else:
+        print("something")
